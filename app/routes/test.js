e42a6da441de1dfca45d1f7af6f03ff62d78ee20
Abeer Waseem <abeer.waseem1@gmail.com>
2017-04-20 00:03:03 +0500

    Implementation of graph

--- app/routes/test.js
+++ app/routes/test.js
@@ -13,13 +13,17 @@ var jsonf = jsonfile.readFileSync(file);
 var data = jsonf.nodes;
 
 var chars = jsonf.Links;
-
+var links = [];
 var matrixArray = [chars.length];
 
 var results = [];
 
 for(var k = 0; k < chars.length ; k++){
 	matrixArray[k] = chars[k].Source;
+	links.push({ 
+	        "index" : k,
+	        "value"  :  chars[k].Source
+	    });
 }
 
 //console.log(matrixArray[1]);
@@ -59,12 +63,21 @@ for(var i = 0; i < n; i++){
 	// if (longerLength == 0) {
 	// 	json[i] = {"Ground" : s1, "Prediction": s2, "Match" : 1.0};
 	// }
-	results[i] = [shorterLength];
+	//results[i] = [shorterLength];
+	//var obj = '{';
 	for(var j=0; j < shorterLength; j++){
-
-		results[i][j] = [2];
+		results.push({ 
+	        "x" : matrixArray.indexOf(s1Array[j]),
+	        "y"  :  matrixArray.indexOf(s2Array[j])
+	       /* "x" : s1Array[j],
+	        "y"  :  s2Array[j]*/
+	    });
+		/*results[i][j] = [2];
 		results[i][j][0] = s1Array[j];
-		results[i][j][1] = s2Array[j];
+		results[i][j][1] = s2Array[j];*/
+
+		// obj += 'x : '+s1Array[j]+',';
+		// obj += 'y : '+s2Array[j];
 
 		// if(s1Array[j] == s2Array[j]){
 		// 	console.log("Matched : " + s1Array[j] +' , '+ s2Array[j]);
@@ -72,6 +85,8 @@ for(var i = 0; i < n; i++){
 		// 	console.log("Not Matched : " + s1Array[j] +' , '+ s2Array[j]);
 		// }
 	}
+
+	//obj += '}';
 	
 
 	// 	//array[i] = s1.split(' ');
@@ -81,11 +96,14 @@ for(var i = 0; i < n; i++){
 	
 }
 
+//console.log(results);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('test', 
   	{ title: 'String Comparison', 
-  	  links: matrixArray,
+  	  links: links,
+  	  nodes: matrixArray,
   	  results: results
 	}
   	);
